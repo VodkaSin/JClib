c46c7cde29610a68d462cbb29d46405f47286b99
vodkasin <insomemelodiousplot@gmail.com>
2023-02-01 22:20:19 +0800

    CSS input for piqs

--- solver/piqs_solve.py
+++ solver/piqs_solve.py
@@ -1,3 +1,11 @@
+import os
+import sys
+PROJECT_ROOT = os.path.abspath(os.path.join(
+                  os.path.dirname(__file__), 
+                  os.pardir)
+)
+sys.path.append(PROJECT_ROOT)
+
 import utils.ops as ops
 import qutip
 import qutip.piqs as piqs
@@ -6,10 +14,11 @@ import time
 import matplotlib.pyplot as plt
 
 class sys:
-    def __init__(self, N_spin, N_cav, delta_a, delta_c, gk, E_spin=True):
+    def __init__(self, N_spin, N_cav, delta_a, delta_c, gk, theta, phi):
         self.N_spin = N_spin
         self.N_cav = N_cav
-        self.E_spin = E_spin
+        self.A = np.sin(theta/2)
+        self.B = np.exp(1j*phi)*np.cos(theta/2)
         self.ID_spin = qutip.qeye(self.N_spin)
         self.ID_cav = qutip.qeye(self.N_cav)
 
@@ -36,10 +45,8 @@ class sys:
         self.jpjm_tot = qutip.tensor(qutip.qeye(N_cav), jp*jm)
         self.a_tot = qutip.tensor(a, qutip.qeye(nds))
         self.ada_tot = qutip.tensor(a.dag()*a, qutip.qeye(nds))
-        if self.E_spin:
-            self.psi0 = qutip.tensor(qutip.fock_dm(N_cav,0), piqs.excited(N_spin, basis='dicke'))
-        else:
-            self.psi0 = qutip.tensor(qutip.fock_dm(N_cav,0), piqs.ground(N_spin, basis='dicke'))
+        self.psi0 = qutip.tensor(qutip.fock_dm(N_cav,0), piqs. css(self.N_spin, self.A, self.B))
+
         # Hamiltonian
         H_spin = delta_a/2 * self.jz_tot
         H_cav = delta_c * self.a_tot.dag() * self.a_tot
@@ -47,11 +54,9 @@ class sys:
         self.H = [H_spin+H_cav+H_int] 
         
     def __str__(self):
-        if self.E_spin:
-            return f'PIQS solver for {self.N_spin} excited spins, Fock states = {self.N_cav}'
-        else:
-            return f'PIQS solver for {self.N_spin} ground state spins, Fock states = {self.N_cav}'
-    
+        return f'PIQS solver for {self.N_spin} spins, {self.A}|e>+{self.B}|g>. Fock states = {self.N_cav}'
+
+
     def run_mesolve(self, tlist, c_ops, e_ops):
         start = time.time()
         out = qutip.mesolve(self.H, self.psi0, tlist, c_ops = c_ops, e_ops = e_ops)
@@ -62,7 +67,15 @@ class sys:
 
 if __name__ == "__main__":
     t = np.linspace(0,2,1000)
-    p = sys( 3, 20, 0, 0, 1.6)
-    p_out = p.run_mesolve(t, [np.sqrt(10)*p.a_tot], [p.jz_tot, p.ada_tot])
-    plt.plot(t, p_out.expect[0])
+    p1 = sys( 5, 20, 0, 0, 1.6, np.pi, 0)
+    p2 = sys( 5, 20, 0, 0, 1.6, 0.8*np.pi, 0)
+    p3 = sys( 5, 20, 0, 0, 1.6, 0.9*np.pi, np.pi/2)
+    p_out1 = p1.run_mesolve(t, [np.sqrt(10)*p1.a_tot], [p1.jz_tot, p1.ada_tot])
+    p_out2 = p2.run_mesolve(t, [np.sqrt(10)*p2.a_tot], [p2.jz_tot, p2.ada_tot])
+    p_out3 = p3.run_mesolve(t, [np.sqrt(10)*p3.a_tot], [p3.jz_tot, p3.ada_tot])
+    plt.plot(t, p_out1.expect[0], label="1,0")
+    plt.plot(t, p_out2.expect[0], label="0.8,0")
+    plt.plot(t, p_out3.expect[0], label="0.9,0.5")
+    plt.legend()
     plt.show()
+    
