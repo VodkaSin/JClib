aade1e0056427cb4a56595d3775743e3e5df7fec
vodkasin <insomemelodiousplot@gmail.com>
2023-02-01 18:20:24 +0800

    solver folder created

--- solver/exact_solve.py
+++ solver/exact_solve.py
@@ -1,3 +1,11 @@
+import os
+import sys
+PROJECT_ROOT = os.path.abspath(os.path.join(
+                  os.path.dirname(__file__), 
+                  os.pardir)
+)
+sys.path.append(PROJECT_ROOT)
+
 import utils.ops as ops
 import qutip
 import qutip.piqs as piqs
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,4 @@
-import utils.ops
-import utils.stats
-import utils.sci_cal
-
+from utils.sci_cal import unit_time, delay_time
+from utils.ops import ind_op, sum_ops
+from utils.stats import gen_rand_pop, gen_same_pop, gauss, ind_where
 print("utils imported")
