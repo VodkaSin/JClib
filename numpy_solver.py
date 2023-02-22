0a7b76ae28679d4ed201de2b75e39464141c10ed
vodkasin <insomemelodiousplot@gmail.com>
2023-02-22 15:06:27 +0800

    Added numpy code for testing

--- numpy_solver.py
+++ numpy_solver.py
@@ -1,4 +1,3 @@
-import cupy as cp
 import numpy as np
 from numpy.random import seed, normal
 import matplotlib.pyplot as plt
@@ -66,10 +65,10 @@ def delay_time(gk, N_spin, kappa, theta):
 class sys:
     def __init__(self, pop_inclass, delta_a, delta_c, gk, theta, phi, cav_decay, spin_decay, spin_dephase):
         """
-        Initialize basic icputs of the system
-        pop_inclass : cp.array
+        Initialize basic inputs of the system
+        pop_inclass : np.array
             Contains the number of spins in each class arranged by index
-        delta_a : cp.array
+        delta_a : np.array
             Contains the detuning of the spins in each class arranged by index
         delta_c : float
             Cavity detuning from pump laser
@@ -86,7 +85,7 @@ class sys:
             Local spin dephase rate
         """
         self.pop_inclass = pop_inclass
-        self.k = cp.size(pop_inclass) # Total number of classes (ensembles)
+        self.k = np.size(pop_inclass) # Total number of classes (ensembles)
         self.delta_a = delta_a
         self.delta_c = delta_c
         self.gk = gk
@@ -98,7 +97,7 @@ class sys:
         
         # Convenient parameters to use
         self.w_spin = self.delta_a - 1j*(self.gamma/2 + self.Gamma)
-        self.w_spin_r = cp.tile(self.w_spin, self.k).reshape(self.k, self.k) # For convenience
+        self.w_spin_r = np.tile(self.w_spin, self.k).reshape(self.k, self.k) # For convenience
         self.w_cav = self.delta_c - 0.5j*self.kappa
 
         # Cavity pumping is set to 0 by default
@@ -111,30 +110,30 @@ class sys:
         """
         Inizializes the mean field matrices
         """
-        G = cp.cos(self.theta/2) # Coefficient of ground state
-        E = cp.sin(self.theta/2)*cp.exp(1j*self.phi) # Coefficient of excited state
+        G = np.cos(self.theta/2) # Coefficient of ground state
+        E = np.sin(self.theta/2)*np.exp(1j*self.phi) # Coefficient of excited state
 
         self.a = 0.+ 0j
-        self.ad = cp.conjugate(self.a)
+        self.ad = np.conjugate(self.a)
         self.ada = 0.
         self.a2 = 0.
 
-        self.sz = -cp.cos(self.theta)*cp.ones(self.k)
-        self.sm = G*E*cp.ones(self.k)
-        self.sp = cp.conjugate(E)*G*cp.ones(self.k)
+        self.sz = -np.cos(self.theta)*np.ones(self.k)
+        self.sm = G*E*np.ones(self.k)
+        self.sp = np.conjugate(E)*G*np.ones(self.k)
 
-        self.a_sz = cp.zeros(self.k, dtype=np.cfloat)
-        self.a_sm = cp.zeros(self.k, dtype=np.cfloat)
-        self.a_sp = cp.zeros(self.k, dtype=np.cfloat)
+        self.a_sz = np.zeros(self.k, dtype=np.cfloat)
+        self.a_sm = np.zeros(self.k, dtype=np.cfloat)
+        self.a_sp = np.zeros(self.k, dtype=np.cfloat)
 
-        self.sz_sm = 0.25*cp.sin(2*self.theta)*cp.exp(-1j*self.phi)\
-                     * cp.ones((self.k, self.k), dtype=np.cfloat)
-        self.sz_sz = cp.cos(self.theta)**2\
-                     * cp.ones((self.k, self.k), dtype=np.cfloat)
-        self.sm_sm = 0.25*cp.sin(self.theta)**2\
-                     * cp.ones((self.k, self.k), dtype=np.cfloat)
-        self.sp_sm = G**2* cp.abs(E)**2\
-                     * cp.ones((self.k, self.k), dtype=np.cfloat)
+        self.sz_sm = 0.25*np.sin(2*self.theta)*np.exp(-1j*self.phi)\
+                     * np.ones((self.k, self.k), dtype=np.cfloat)
+        self.sz_sz = np.cos(self.theta)**2\
+                     * np.ones((self.k, self.k), dtype=np.cfloat)
+        self.sm_sm = 0.25*np.sin(self.theta)**2\
+                     * np.ones((self.k, self.k), dtype=np.cfloat)
+        self.sp_sm = G**2* np.abs(E)**2\
+                     * np.ones((self.k, self.k), dtype=np.cfloat)
     
     """
     Functions to calculate mean-field increments at each time step
@@ -145,89 +144,89 @@ class sys:
                  * self.sm) - 1j * self.F) 
     
     def cal_da2(self):
-        return (-2j * self.w_cav * self.a2 - 2j * self.gk * cp.sum(self.pop_inclass
+        return (-2j * self.w_cav * self.a2 - 2j * self.gk * np.sum(self.pop_inclass
                  * self.a_sm) - 2j * self.F * self.a)
     
     def cal_dada(self):
         """
         Returns a real number: Intrafield cavity photons
         """
-        return cp.real(-2 * self.gk * cp.sum(self.pop_inclass * cp.imag(self.a_sp))
-                 - 2 * self.F * cp.imag(self.a) - self.kappa * self.ada)
+        return np.real(-2 * self.gk * np.sum(self.pop_inclass * np.imag(self.a_sp))
+                 - 2 * self.F * np.imag(self.a) - self.kappa * self.ada)
 
     def cal_dsz(self):
         """
         Returns a real array (self.k): Average spin inversion in each class
         """
-        return cp.real(4 * self.gk * cp.imag(self.a_sp) - self.gamma * (1 + self.sz))
+        return np.real(4 * self.gk * np.imag(self.a_sp) - self.gamma * (1 + self.sz))
     
     def cal_dsm(self):
         return (-1j * self.w_spin * self.sm + 1j * self.a_sz * self.gk)
     
     def cal_da_sz(self):
         return (-1j * self.w_cav * self.a_sz - 1j * self.gk * (-self.sm 
-                 - cp.diagonal(self.sz_sm)) - 2j * self.gk * ((self.a2 * self.sp
+                 - np.diagonal(self.sz_sm)) - 2j * self.gk * ((self.a2 * self.sp
                  + 2 * self.a * self.a_sp - 2 * self.a**2 * self.sp)
-                 - (self.ad * self.a_sm + self.a * cp.conjugate(self.a_sp)
-                 + self.ada * self.sm - 2 * cp.abs(self.a)**2 * self.sm))
+                 - (self.ad * self.a_sm + self.a * np.conjugate(self.a_sp)
+                 + self.ada * self.sm - 2 * np.abs(self.a)**2 * self.sm))
                  - self.gamma * (self.a + self.a_sz) - 1j * self.F * self.sz
-                 - 1j * self.gk * cp.sum(self.sz_sm * self.pop_inclass[..., None], axis=0))
+                 - 1j * self.gk * np.sum(self.sz_sm * self.pop_inclass[..., None], axis=0))
     
     def cal_da_sm(self):
         return (-1j * (self.w_spin + self.w_cav) * self.a_sm 
-                 + 1j * self.gk * (cp.diagonal(self.sm_sm) + 2 * self.a_sz * self.a 
+                 + 1j * self.gk * (np.diagonal(self.sm_sm) + 2 * self.a_sz * self.a 
                  + self.a2 * self.sz - 2 * self.a**2 * self.sz) 
-                 - 1j * self.F * self.sm - 1j * self.gk * cp.sum(self.sm_sm 
+                 - 1j * self.F * self.sm - 1j * self.gk * np.sum(self.sm_sm 
                  * self.pop_inclass[..., None], axis=0))
 
     def cal_da_sp(self):
-        return (1j * (cp.conj(self.w_spin) - self.w_cav) * self.a_sp 
-                 - .5j * self.gk * (1 - self.sz) + 1j * self.gk * cp.diagonal(self.sp_sm)
-                 -1j * self.gk * cp.sum(self.sp_sm * self.pop_inclass[..., None], axis=0) 
+        return (1j * (np.conj(self.w_spin) - self.w_cav) * self.a_sp 
+                 - .5j * self.gk * (1 - self.sz) + 1j * self.gk * np.diagonal(self.sp_sm)
+                 -1j * self.gk * np.sum(self.sp_sm * self.pop_inclass[..., None], axis=0) 
                  - 1j * self.gk * ((1 + self.ada) * self.sz 
-                 + 2 * cp.real(self.a * cp.conjugate(self.a_sz))
-                 - 2 * cp.abs(self.a)**2 * self.sz) -1j * self.F * self.sp)
+                 + 2 * np.real(self.a * np.conjugate(self.a_sz))
+                 - 2 * np.abs(self.a)**2 * self.sz) -1j * self.F * self.sp)
 
     def cal_dsm_sm(self):
         return (-1j * (self.w_spin_r+self.w_spin_r.T) * self.sm_sm 
                  + 1j * self.gk * (self.a * self.sz_sm
-                 + self.a_sz * cp.vstack(self.sm) + self.sz * cp.vstack(self.a_sm)
-                 - self.a * self.sz * cp.vstack(self.sm)) 
-                 + 1j * self.gk * (self.a * cp.conjugate(self.sz_sm).T 
-                 + self.a_sm * cp.vstack(self.sz) + self.sm * cp.vstack(self.a_sz)
-                 - 2 * self.a * self.sm * cp.vstack(self.sz)))
+                 + self.a_sz * np.vstack(self.sm) + self.sz * np.vstack(self.a_sm)
+                 - self.a * self.sz * np.vstack(self.sm)) 
+                 + 1j * self.gk * (self.a * np.conjugate(self.sz_sm).T 
+                 + self.a_sm * np.vstack(self.sz) + self.sm * np.vstack(self.a_sz)
+                 - 2 * self.a * self.sm * np.vstack(self.sz)))
     
     def cal_dsz_sz(self):
-        return (4 * self.gk * ((self.a_sp * cp.vstack(self.sz) 
-                 + self.sp * cp.vstack(self.a_sz) + self.a * cp.conjugate(self.sz_sm).T 
-                 - 2 * self.a * self.sp * cp.vstack(self.sz))
-                 + (self.a_sp.T * self.sz + self.a_sz * cp.vstack(self.sp)
-                 + self.a * cp.conjugate(self.sz_sm) 
-                 - 2 * self.a * cp.vstack(self.sp) * self.sz))
-                 + self.gamma * (cp.tile(self.sz, self.k).reshape(self.k, self.k).T
+        return (4 * self.gk * ((self.a_sp * np.vstack(self.sz) 
+                 + self.sp * np.vstack(self.a_sz) + self.a * np.conjugate(self.sz_sm).T 
+                 - 2 * self.a * self.sp * np.vstack(self.sz))
+                 + (self.a_sp.T * self.sz + self.a_sz * np.vstack(self.sp)
+                 + self.a * np.conjugate(self.sz_sm) 
+                 - 2 * self.a * np.vstack(self.sp) * self.sz))
+                 + self.gamma * (np.tile(self.sz, self.k).reshape(self.k, self.k).T
                  + self.sz_sz))
 
     def cal_dsz_sm(self):
         return ((-1j * self.w_spin_r + self.gamma) * self.sz_sm 
-                 + 1j * self.gk * (self.a_sz * cp.vstack(self.sz)
-                 + cp.vstack(self.a_sz)*self.sz + self.a * self.sz_sz
-                 - 2 * self.a * self.sz * cp.vstack(self.sz)) 
-                 - 2j * self.gk * (self.a_sp * cp.vstack(self.sm) 
-                 + cp.vstack(self.a_sm) * self.sp + self.a * self.sp_sm
-                 - 2 * self.a * self.sp * cp.vstack(self.sm)
-                 - (cp.conjugate(self.a_sp) * cp.vstack(self.sm) 
-                 + cp.conjugate(self.a_sp).T * self.sm + self.ad * self.sm_sm
-                 - 2 * self.ad * self.sm * cp.vstack(self.sm)))
-                 - self.gamma * cp.tile(self.sm, self.k).reshape(self.k, self.k).T)
+                 + 1j * self.gk * (self.a_sz * np.vstack(self.sz)
+                 + np.vstack(self.a_sz)*self.sz + self.a * self.sz_sz
+                 - 2 * self.a * self.sz * np.vstack(self.sz)) 
+                 - 2j * self.gk * (self.a_sp * np.vstack(self.sm) 
+                 + np.vstack(self.a_sm) * self.sp + self.a * self.sp_sm
+                 - 2 * self.a * self.sp * np.vstack(self.sm)
+                 - (np.conjugate(self.a_sp) * np.vstack(self.sm) 
+                 + np.conjugate(self.a_sp).T * self.sm + self.ad * self.sm_sm
+                 - 2 * self.ad * self.sm * np.vstack(self.sm)))
+                 - self.gamma * np.tile(self.sm, self.k).reshape(self.k, self.k).T)
 
     def cal_dsp_sm(self):
         return (-1j * (self.w_spin_r - self.w_spin_r.T) * self.sp_sm
-                 + 1j * (self.a_sp * cp.vstack(self.sz) 
-                 + cp.vstack(self.a_sz) * self.sp + self.a * cp.conjugate(self.sz_sm).T
-                 - 2 * self.a * self.sp * cp.vstack(self.sz))
-                 - 1j * (cp.conjugate(self.a_sz) * cp.vstack(self.sm)
-                 + cp.conjugate(self.a_sp).T * self.sz + self.ad * self.sz_sm
-                 - 2 * self.ad * self.sz * cp.vstack(self.sm)))
+                 + 1j * (self.a_sp * np.vstack(self.sz) 
+                 + np.vstack(self.a_sz) * self.sp + self.a * np.conjugate(self.sz_sm).T
+                 - 2 * self.a * self.sp * np.vstack(self.sz))
+                 - 1j * (np.conjugate(self.a_sz) * np.vstack(self.sm)
+                 + np.conjugate(self.a_sp).T * self.sz + self.ad * self.sz_sm
+                 - 2 * self.ad * self.sz * np.vstack(self.sm)))
     
     def update(self, F, dt):
         """
@@ -236,13 +235,13 @@ class sys:
         self.F = F
 
         self.a += self.cal_da() * dt
-        self.ad = cp.conjugate(self.a)
+        self.ad = np.conjugate(self.a)
         self.ada += self.cal_dada() * dt
         self.a2 += self.cal_da2() * dt
 
         self.sz += self.cal_dsz() * dt
         self.sm += self.cal_dsm() * dt
-        self.sp = cp.conjugate(self.sm)
+        self.sp = np.conjugate(self.sm)
 
         self.a_sz += self.cal_da_sz() * dt
         self.a_sm += self.cal_da_sm() * dt
@@ -262,22 +261,22 @@ class sys:
             Cavity pump amplitude with respect to time
         """
         intervals = np.size(tlist)
-        F_t = cp.zeros(intervals)
+        F_t = np.zeros(intervals)
         dt = tlist[-1]/intervals
-        e_ada = cp.zeros(intervals)
-        e_sz = cp.zeros((intervals, self.k))
-        e_sp_sm = cp.zeros((intervals, self.k))
+        e_ada = np.zeros(intervals)
+        e_sz = np.zeros((intervals, self.k))
+        e_sp_sm = np.zeros((intervals, self.k))
         print("Start solving, dt = ", dt)
         start = time.time()
         for t in range(intervals):
             self.update(F_t[t], dt)
             e_ada[t] = self.ada
             e_sz[t] = self.sz
-            e_sp_sm[t] = cp.diagonal(self.sp_sm)
+            e_sp_sm[t] = np.diagonal(self.sp_sm)
         end = time.time()
         print(end-start)
 
-        return [cp.asnumpy(e_ada), cp.asnumpy(e_sz), cp.asnumpy(e_sp_sm)]
+        return [e_ada, e_sz, e_sp_sm]
 
 
     def solve_adapt(self, endtime, F, min_dt=1e-6):
@@ -295,7 +294,7 @@ class sys:
         time_now = 0
         loop_count = 0
         dt = max(0.25 * 1/max(self.kappa, self.gamma, self.Gamma,
-                 0.5 * self.gk * max(self.pop_inclass) * cp.sqrt(max(self.pop_inclass)))
+                 0.5 * self.gk * max(self.pop_inclass) * np.sqrt(max(self.pop_inclass)))
                  , min_dt) # default by fastest oscillation or a set minimum
         print("Start solving, dt = ", dt)
         start = time.time()
@@ -308,11 +307,11 @@ class sys:
         print(end-start)
 
 if __name__ == "__main__":
-    pop_inclass = cp.asarray([1000,1000,1000,1000])
-    delta_a = cp.asarray([20,0,50,30])
+    pop_inclass = np.asarray([1000 for i in range(2)])
+    delta_a = np.linspace(0,50,2)
     delta_c = 0
     gk = 1.6
-    theta = cp.pi
+    theta = np.pi
     phi = 0
     cav_decay = 160
     spin_decay = 0
@@ -321,12 +320,12 @@ if __name__ == "__main__":
     test_sys = sys(pop_inclass, delta_a, delta_c, gk, theta, phi, 
                  cav_decay, spin_decay, spin_dephase)
     
-    num_iter = 1e4
+    num_iter = 1e6
     tlist = np.linspace(0,0.2,int(num_iter))
-    F_t = cp.zeros(int(num_iter))
+    F_t = np.zeros(int(num_iter))
     results = test_sys.solve_constant(tlist)
     Td_theory = delay_time(gk, 100, cav_decay, theta)
-    Td_simulate = ind_where(results[1], 0.0, 0.05)
+    Td_simulate = ind_where(results[1], 0.0, 0.5)
     print(Td_theory, Td_simulate)
 
     fig, ax = plt.subplots(3,1, sharex='col')
@@ -341,4 +340,4 @@ if __name__ == "__main__":
     ax[1].plot(tlist, results[1])
     ax[2].plot(tlist, results[2])
 
-    plt.savefig("cupy.png")
+    plt.savefig("np.png")
